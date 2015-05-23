52874450f7fd981bbb037057804b4efaaedae5c5
Stefan Herrnleben <s.herrnleben@syslex.de>
2015-05-23 19:06:51 +0200

    redirect to belonet start page from default controller

--- Classes/Belonet/Ssn/Controller/StandardController.php
+++ Classes/Belonet/Ssn/Controller/StandardController.php
@@ -14,9 +14,7 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	 * @return void
 	 */
 	public function indexAction() {
-		$this->view->assign('foos', array(
-			'bar', 'baz'
-		));
+		$this->redirect('index','Mainpage');
 	}
 
 }
