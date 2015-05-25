cf318ca7029d5358a92624824d125ccf98b26ed7
Stefan Herrnleben <s.herrnleben@syslex.de>
2015-05-25 22:36:27 +0200

    some color optimization and new icons for wiki and assistance

--- Classes/Belonet/Ssn/Controller/MainpageController.php
+++ Classes/Belonet/Ssn/Controller/MainpageController.php
@@ -11,17 +11,18 @@ use TYPO3\Flow\Annotations as Flow;
 class MainpageController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 
 	/**
-     * this will activate the Index.html in Directory Mainpage
-     * here comes the content for Mainpage
-     *
+	 * this will activate the Index.html in Directory Mainpage
+	 * here comes the content for Mainpage
+	 *
 	 * @return void
 	 */
 	public function indexAction() {
 
 	}
 
-    public function showImpressumAction(){
+	public function showImpressumAction() {
 
-    }
+	}
+
 
 }
