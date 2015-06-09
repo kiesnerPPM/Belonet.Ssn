0fff7c3bdfbedad6623be18264645eb01c38461a
Veronika <veronika.lesch@informatik.uni-wuerzburg.de>
2015-06-09 17:12:19 +0200

    changed usernames and added more useraccounts

--- Classes/Belonet/Ssn/Controller/SetupController.php
+++ Classes/Belonet/Ssn/Controller/SetupController.php
@@ -34,7 +34,7 @@ class SetupController extends \TYPO3\Flow\Mvc\Controller\ActionController {
            $account->setPassword("password");
 
            $account->setUsername("Marwin");
-           $account->setEmailAddress("marwin@email.com");
+           $account->setEmailAddress("marwin@belonet.de");
 
            $this->accountRepository->add($account->getPrimaryAccount());
            $this->userAccountRepository->add($account);
@@ -43,7 +43,7 @@ class SetupController extends \TYPO3\Flow\Mvc\Controller\ActionController {
            $account2->setPassword("password");
 
            $account2->setUsername("Stefan");
-           $account2->setEmailAddress("stefan@email.com");
+           $account2->setEmailAddress("stefan@belonet.de");
 
            $this->accountRepository->add($account2->getPrimaryAccount());
            $this->userAccountRepository->add($account2);
@@ -51,11 +51,38 @@ class SetupController extends \TYPO3\Flow\Mvc\Controller\ActionController {
            $account3 = new \Belonet\Ssn\Domain\Model\UserAccount();
            $account3->setPassword("password");
 
-           $account3->setUsername("veronika");
-           $account3->setEmailAddress("veronika@email.com");
+           $account3->setUsername("Veronika");
+           $account3->setEmailAddress("veronika@belonet.de");
 
            $this->accountRepository->add($account3->getPrimaryAccount());
            $this->userAccountRepository->add($account3);
+
+           $account4 = new \Belonet\Ssn\Domain\Model\UserAccount();
+           $account4->setPassword("password");
+
+           $account4->setUsername("Max");
+           $account4->setEmailAddress("max@belonet.de");
+
+           $this->accountRepository->add($account4->getPrimaryAccount());
+           $this->userAccountRepository->add($account4);
+
+           $account5 = new \Belonet\Ssn\Domain\Model\UserAccount();
+           $account5->setPassword("password");
+
+           $account5->setUsername("Frank");
+           $account5->setEmailAddress("frank@belonet.de");
+
+           $this->accountRepository->add($account5->getPrimaryAccount());
+           $this->userAccountRepository->add($account5);
+
+           $account6 = new \Belonet\Ssn\Domain\Model\UserAccount();
+           $account6->setPassword("password");
+
+           $account6->setUsername("Anna");
+           $account6->setEmailAddress("anna@belonet.de");
+
+           $this->accountRepository->add($account6->getPrimaryAccount());
+           $this->userAccountRepository->add($account6);
        }
        $this->persistenceManager->persistAll();
        $this->addFlashMessage('Created Default Accounts');
