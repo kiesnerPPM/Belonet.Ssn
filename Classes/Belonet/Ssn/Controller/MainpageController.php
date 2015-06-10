e2e524b3b88aaa3830dea197508062db0baf11ff
Stefan Herrnleben <s.herrnleben@syslex.de>
2015-06-10 15:41:36 +0200

    mainpage finished

--- Classes/Belonet/Ssn/Controller/MainpageController.php
+++ Classes/Belonet/Ssn/Controller/MainpageController.php
@@ -10,6 +10,12 @@ use TYPO3\Flow\Annotations as Flow;
 
 class MainpageController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 
+  /**
+   * @Flow\Inject
+   * @var \TYPO3\Flow\Security\Authentication\AuthenticationManagerInterface
+   */
+  protected $authenticationManager;
+
 	/**
 	 * this will activate the Index.html in Directory Mainpage
 	 * here comes the content for Mainpage
@@ -17,7 +23,17 @@ class MainpageController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	 * @return void
 	 */
 	public function indexAction() {
-
+    $user = $this->authenticationManager->getSecurityContext()->getAccount()->getParty();
+    $hour = date('H');
+    if ($hour < 6)
+      $welcome = 'Gute Nacht '.$user->getName().',';
+    else if ($hour < 12)
+      $welcome = 'Guten Morgen '.$user->getName().',';
+    else if ($hour < 18)
+      $welcome = 'Guten Tag '.$user->getName().',';
+    else
+      $welcome = 'Guten Abend '.$user->getName().',';
+    $this->view->assign('welcome',$welcome);
 	}
 
   public function showImpressumAction() {
