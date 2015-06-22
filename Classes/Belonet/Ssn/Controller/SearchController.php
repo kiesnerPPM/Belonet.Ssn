756187b573c011f6b0a2d76e699fcedf619b7079
Frank <frank.loh@gmx.de>
2015-06-22 23:23:30 +0200

    started myData

--- Classes/Belonet/Ssn/Controller/SearchController.php
+++ Classes/Belonet/Ssn/Controller/SearchController.php
@@ -13,10 +13,30 @@ class SearchController extends \TYPO3\Flow\Mvc\Controller\ActionController {
     /**
      * action for displaying standard search page
      */
-    public function showSearchPageAction(){
+    protected $persistenceManager;
+    protected $searchentryRepository;
+
+
+    /**
+     * @param \Belonet\Ssn\Domain\Model\Searchentry $currentEntry
+     */
+    public function showSearchPageAction(\Belonet\Ssn\Domain\Model\Searchentry $currentEntry = null) {
+        $user = $this->authenticationManager->getSecurityContext()->getAccount()->getParty();
+
+        $allEntries = $this->searchentryRepository->findAll()->toArray();
+        if (is_null($currentEntry)) {
+            $currentEntry = $allEntries[0];
+        }
+        $this->view->assign("currentEntry", $currentEntry);
+        $this->view->assign("allEntries", $allEntries);
+
 
     }
-
+    public function createNewEntryAction(\Belonet\Ssn\Domain\Model\Searchentry $entry)
+    {
+        $this->searchentryRepository->add($entry);
+        $this->redirect("showSearchPage", null, null, array());
+    }
     /**
      * action for displaying search results
      */
